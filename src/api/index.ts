44909d2e435a60477e58de866a4c4f863a32cd77
Jakub Wawrzyniak <jakubwawrzyniak6@gmail.com>
2023-12-24 21:36:06 +0100

    Bench frontend request-response time

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,14 +12,20 @@ export type JuliaImageRequest = {
   width_px: number;
 };
 
+// const id = (req: JuliaImageRequest) => {
+//   const a = req.bottom_right;
+//   const b = req.top_left;
+//   return `${a.imaginary} ${a.real} ${b.imaginary} ${b.real}`;
+// };
+
 export const calcImage = async (
   request: JuliaImageRequest
 ): Promise<ImageData> => {
-  console.time("invoke");
+  // console.time(`invoke ${id(request)}`);
   const pixels = await invoke<number[]>("calc_image", { request });
-  console.timeEnd("invoke");
-  console.time("transform");
+  // console.timeEnd(`invoke ${id(request)}`);
+  // console.time(`transform ${id(request)}`);
   const image = pixelsToImage(pixels, request.width_px);
-  console.timeEnd("transform");
+  // console.timeEnd(`transform ${id(request)}`);
   return image;
 };
