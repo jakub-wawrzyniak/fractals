1a9dd2f20c09d4375269f45cc2266bd444586098
Kuba Wawrzyniak <kubawawrzyniak@proton.me>
2024-02-26 16:46:01 +0100

    Add hue detection from rgb

--- src-tauri/src/color/hsl.rs
+++ src-tauri/src/color/hsl.rs
@@ -6,20 +6,19 @@ use crate::data::Rgb;
 fn hue_to_rgb(p: f64, q: f64, mut t: f64) -> f64 {
     if t < 0.0 {
         t += 1.0
-    };
-    if t > 1.0 {
+    } else if t > 1.0 {
         t -= 1.0
     };
+
     if t < 1.0 / 6.0 {
-        return p + (q - p) * 6.0 * t;
+        p + (q - p) * 6.0 * t
+    } else if t < 0.5 {
+        q
+    } else if t < 2.0 / 3.0 {
+        p + (q - p) * (2.0 / 3.0 - t) * 6.0
+    } else {
+        p
     }
-    if t < 0.5 {
-        return q;
-    }
-    if t < 2.0 / 3.0 {
-        return p + (q - p) * (2.0 / 3.0 - t) * 6.0;
-    }
-    p
 }
 
 pub fn hsl_to_rgb(h: f64, s: f64, l: f64) -> Rgb {
@@ -57,8 +56,33 @@ pub fn gradient(step: f64) -> Rgb {
     hsl_to_rgb(h, s, l)
 }
 
+fn rgb_to_hue(rgb: &Rgb) -> f64 {
+    let r = (rgb.0[0] as f64) / 255.0;
+    let g = (rgb.0[1] as f64) / 255.0;
+    let b = (rgb.0[2] as f64) / 255.0;
+
+    let largest = r.max(g).max(b);
+    let smallest = r.min(g).min(g);
+    let delta = largest - smallest;
+    if delta == 0.0 {
+        return 0.0; // Grayscale, saturation == 0
+    }
+
+    const SCALE: f64 = 1.0 / 6.0;
+    if largest == r {
+        SCALE * (((g - b) / delta) % 6.0)
+    } else if largest == g {
+        SCALE * (((b - r) / delta) + 2.0)
+    } else if largest == b {
+        SCALE * (((r - g) / delta) + 4.0)
+    } else {
+        panic!("Comparison error (neither is largest)")
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    #[allow(unused)]
     use super::*;
 
     #[test]
@@ -120,5 +144,19 @@ mod tests {
         let out = transition(from, to, step);
 
         assert_eq!(out, 20.0);
+    }
+
+    #[test]
+    fn detects_red() {
+        let red = Rgb::from([255, 0, 0]);
+        let hue = rgb_to_hue(&red);
+        assert_eq!(hue, 0.0);
+    }
+
+    #[test]
+    fn detects_blue() {
+        let red = Rgb::from([0, 0, 255]);
+        let hue = rgb_to_hue(&red);
+        assert_eq!(hue, 2.0 / 3.0);
     }
 }
